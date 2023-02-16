1b2dccfeb567583436b01bbb8fc778da0a57edf6
Titus Lim <titus.happy@gmail.com>
2023-02-16 12:15:59 +0800

    Error handling

--- Bot/bot.py
+++ Bot/bot.py
@@ -3,12 +3,13 @@ from discord.ext import commands
 
 # Constants
 description = '''Personal Discord Bot Testing'''
+command_prefix = '#'
 
 def bot():
     intents = discord.Intents.default()
     intents.members = True
     intents.message_content = True
 
-    bot = commands.Bot(command_prefix='#',
+    bot = commands.Bot(command_prefix=command_prefix,
                     description=description, intents=intents)
     return bot
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from discord.ext import commands
 import random
 import sys
 
+# Constants
+ERROR_MISSINGARG = 'Command is missing argument(s) - {argument}'
+
 # Configure sys.path
 sys.path.append(r'./Commands')
 sys.path.append(r'./Bot')
@@ -17,44 +20,33 @@ from bot import bot
 bot = bot()
 
 # Setup Bot Commands
+# On Ready
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
 
-@bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
+# Simple Ping Pong
+@bot.command(name='ping')
+async def ping(ctx):
+    """Ping Pong!"""
+    await ctx.send('Pong!')
 
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
 
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
-
-@bot.command()
-async def repeat(ctx, times: int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await ctx.send(content)
-
-@bot.command()
+# Utility Commands
+# Date Joined
+@bot.command(name='joined')
 async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+@joined.error
+async def joined_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send('Guild Member not found!')
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(ERROR_MISSINGARG.format(argument='Guid Member'))
 
+# Subcommand Checker
 @bot.group()
 async def cool(ctx):
     """Says if a user is cool.
@@ -67,6 +59,26 @@ async def cool(ctx):
 async def _bot(ctx):
     """Is the bot cool?"""
     await ctx.send('Yes, the bot is cool.')
+
+
+# Main Commands
+# Coin Flip
+@bot.command(name='coinflip')
+async def coin_flip(ctx):
+    """Heads of Tails Coin Flip"""
+    flip = random.randint(0, 1)
+    await ctx.send('Heads' if flip == 0 else 'Tails')
+
+@bot.command(name='choose')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+@choose.error
+async def choose_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send('Error: Disallowed input type')
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(ERROR_MISSINGARG.format(argument='Choices(string)'))
 
 # Run Bot
 bot.run(DISCORDBOT_TOKEN)
